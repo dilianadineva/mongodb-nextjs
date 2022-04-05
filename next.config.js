0d95cf1bfac941064ec01aabbf8eed3611de63ce
Dilyana <mnyaha@gmail.com>
2022-04-05 10:40:51 +0300

    added cors headers in next.config.js

--- next.config.js
+++ next.config.js
@@ -5,14 +5,20 @@ const nextConfig = {
     MONGO_URL:
       'mongodb+srv://nextjs:nextjs@nextjs-mongodb.qb2nw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   },
-  async rewrites() {
+  async headers() {
     return [
       {
-        source: '/api/:path*',
-        destination: 'https://mongodb-nextjs.vercel.app/:path*',
-      },
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
     ]
-  },
+  }
 };
 
 module.exports = nextConfig;
